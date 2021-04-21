1aa3f31077a870b275a14b70ff06c802157522da
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-04-21 10:23:52 -0300

    routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import express from "express";
 
 const app = express();
+/**
+ * GET = BUSCAS
+ * POST = CRIAÇÃO
+ * DELETE = DELETAR
+ * PATCH = ALTERAR UMA INFORMAÇÃO
+ * 
+ */
+
+
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
