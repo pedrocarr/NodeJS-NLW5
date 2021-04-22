37a9a259869083040f9dedf6740e1ec2e704c0c6
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-04-21 21:17:54 -0300

    entities done

--- src/entities/Setting.ts
+++ src/entities/Setting.ts
@@ -1,5 +1,7 @@
 import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn } from "typeorm";
 
+import { v4 as uuid  } from "uuid";
+
 @Entity("settings")
 class Setting {
   @PrimaryColumn()
@@ -16,6 +18,12 @@ class Setting {
 
   @CreateDateColumn()
   created_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid();
+    }
+  }
 
 }
 
