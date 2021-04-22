b7265e3658da9e99c6e0862100cd110aec4ca086
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-04-22 08:55:45 -0300

    params and routes

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,19 @@ import { SettingsRepository } from "./repositories/SettingsRepository";
 
 const routes = Router();
 
+/**
+ * Tipos de parâmetros
+ * Routes Params => Parametros de rotas
+ * http://localhost:3333/settings/1
+ * Query Params => Filtros e buscas
+ * http://localhost:3333/settings/1?search=algumacoisa
+ * Body params => {
+ * 
+ * }
+ */
+
 routes.post("/settings", async  (request, response) => {
+  const { chat, username } = request.body;
   const settingsRepository = getCustomRepository(SettingsRepository);
 
   const settings = settingsRepository.create({
