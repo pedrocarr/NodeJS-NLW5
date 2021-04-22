1f069e1d0c88c505f8f705d58f9c19b68e3147a6
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-04-22 09:13:14 -0300

    feat: SettingsController

--- src/routes.ts
+++ src/routes.ts
@@ -16,16 +16,7 @@ const routes = Router();
  */
 
 routes.post("/settings", async  (request, response) => {
-  const { chat, username } = request.body;
-  const settingsRepository = getCustomRepository(SettingsRepository);
-
-  const settings = settingsRepository.create({
-    chat,
-    username
-  })
-  await settingsRepository.save(settings);
-  return response.json(settings);
-  
+ 
 })
 
 export { routes };
