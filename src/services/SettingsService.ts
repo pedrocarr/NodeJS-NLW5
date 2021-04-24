3e624735f0087e12efd10b46a3a543b59d3f4f99
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-04-24 08:04:14 -0300

    configuring settings service

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,17 +1,33 @@
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepository";
 
-interface 
+interface ISettingsCreate {
+  chat: boolean;
+  username: string;
+}
 
 class SettingsService {
 
-  async create() {
+  async create({ chat, username } : ISettingsCreate) {
     
     const settingsRepository = getCustomRepository(SettingsRepository);
+
+    // Select * from settings where username = "username" limit 1; 
+    const userAlreadyExists = await settingsRepository.findOne({
+      username
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
   
     const settings = settingsRepository.create({
       chat,
       username,
     });
     await settingsRepository.save(settings);
+    return settings;
+
   }
 
 }
