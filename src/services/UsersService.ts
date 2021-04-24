2e0d08bac4950447cca71a10dff9f45d1ab2c1ff
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-04-24 08:54:00 -0300

    feat:UsersService

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,14 +1,33 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepository } from "../repositories/UsersRepository"
 
 
 
 class UsersService {
-  async create() {
+  async create(email: string) {
+    const usersRepository = getCustomRepository(UsersRepository);
     // Verificar se usuário existe
 
-    // Se não existir, salvar no DB
+    const userExists = await usersRepository.findOne({
+      email,
+    });
 
     // Se existir, retornar user
+    
+    if (userExists) {
+      return userExists;
+    }
+    
+    
+    const user = usersRepository.create({
+      email,
+    });
+    
+    await usersRepository.save(user);
+    
+    // Se não existir, salvar no DB
 
+    return user;
   }
 
 }
